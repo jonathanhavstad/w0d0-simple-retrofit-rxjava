380229915ce6d145e37ef1e4095c6197f8f931c3
Evin1- <edwin.hdz.12.0@gmail.com>
2016-09-20 20:01:26 -0400

    Add Retrofit with RxJava logic

--- app/src/main/java/com/example/retrofitrxjava/MainActivity.java
+++ app/src/main/java/com/example/retrofitrxjava/MainActivity.java
@@ -2,12 +2,51 @@ package com.example.retrofitrxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.retrofitrxjava.entities.Result;
+
+import java.util.List;
+
+import retrofit2.Retrofit;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<List<Result>> resultObservable = RetrofitHelper.Factory
+                .create("Evin1-");
+
+        resultObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<List<Result>>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d(TAG, "onCompleted: ");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "onError: " + e.getMessage());
+                    }
+
+                    @Override
+                    public void onNext(List<Result> results) {
+                        Log.d(TAG, "onNext: " + results.size());
+                        for (Result result : results) {
+                            Log.d(TAG, "onNext: " + result);
+                        }
+                    }
+                });
     }
 }
